b5ebd5687f73b08507a860c9881f92a64cf6efdf
NicoBarragan <nbarragan01@gmail.com>
2022-08-21 01:30:09 -0300

    feat(withdraw.ts): created withdraw  script for SupplyAave contract

---
+++ scripts/withdraw.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+import { BigNumber, logger } from "ethers";
+import { SupplyAave, SupplyAave__factory } from "../typechain-types";
+
+const { SUPPLY_AAVE_ADDRESS } = process.env;
+const GAS_LIMIT = BigNumber.from("2074000");
+
+export default async function withdraw(
+  lpAmount: BigNumber,
+  tokenAddr: string,
+  userAddr: string
+) {
+  const gas = { gasLimit: GAS_LIMIT };
+
+  const user = await ethers.getSigner(userAddr);
+
+  const supplyContract = (await ethers.getContractAt(
+    SupplyAave__factory.abi,
+    SUPPLY_AAVE_ADDRESS!
+  )) as SupplyAave;
+
+  logger.info("Withdrawing...");
+  const withdrawTx = await supplyContract
+    .connect(user)
+    .withdraw(tokenAddr, lpAmount, gas);
+  await withdrawTx.wait();
+  logger.info("Withdrawed!");
+}
