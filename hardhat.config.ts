a49400958b633d9f91d4dcb3f9364b2ad2592d29
NicoBarragan <nbarragan01@gmail.com>
2022-08-21 01:26:32 -0300

    feat(hardhat.config.ts): added config for multiple networks. added config for executing files by network in .env files. updated compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const found = process.argv.indexOf("--network");
+const networkName = process.argv[found + 1];
+if (!networkName) {
+  throw new Error("invalid network name");
+}
+
+console.log("network", networkName);
+dotenv.config({
+  path: `.env.${networkName}`,
+});
+
+const networkConfig = {
+  url: process.env.URL || "",
+  accounts:
+    process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+};
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.10",
+  networks: {
+    polygon: networkConfig,
+    mumbai: networkConfig,
+    rinkeby: networkConfig,
+    hardhat: {
+      forking: {
+        url: process.env.URL || "",
+      },
+    },
+  },
+  mocha: {
+    timeout: 100000000,
+  },
 };
 
 export default config;
